77199f16dfe9e109be891ee35c5e6bd52ad797cc
Vihr_Kraft <sanefedov2009@gmail.com>
2025-02-10 22:40:34 +0700

    Сделал скрипт совместимым с *nix и Windows

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,4 +1,7 @@
+import os
+
+
 def publish_comics(file_name, chat_id, bot):
-    file_path = f'images/{file_name}'
+    file_path = os.path.join('images', file_name)
     with open(file_path, 'rb') as file:
         bot.send_photo(chat_id=chat_id, photo=file)
