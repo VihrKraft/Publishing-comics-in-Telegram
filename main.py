090fe856ff8c4fbf4fe1ef7f6fb0b62a552b544c
Vihr_Kraft <sanefedov2009@gmail.com>
2025-02-10 22:54:13 +0700

    Разбил код на функции

--- main.py
+++ main.py
@@ -9,12 +9,7 @@ import telegram
 from pathlib import Path
 
 
-def main():
-    load_dotenv()
-    Path("images").mkdir(parents=True, exist_ok=True)
-    chat_id = os.environ['TELEGRAM_CHAT_ID']
-    bot = telegram.Bot(token=os.environ['TELEGRAM_BOT_TOKEN'])
-    url = 'https://xkcd.com/info.0.json'
+def parse_site_xksd(url):
     response = requests.get(url)
     response.raise_for_status() 
     latest_comic_book = response.json()
@@ -24,6 +19,16 @@ def main():
     random_img_response = requests.get(random_img_url)
     random_img_response.raise_for_status()
     comic_book = random_img_response.json()
+    return comic_book
+
+
+def main():
+    load_dotenv()
+    Path("images").mkdir(parents=True, exist_ok=True)
+    chat_id = os.environ['TELEGRAM_CHAT_ID']
+    bot = telegram.Bot(token=os.environ['TELEGRAM_BOT_TOKEN'])
+    url = 'https://xkcd.com/info.0.json'
+    comic_book = parse_site_xksd(url)
     img_url = comic_book['img']
     title_comic_book = comic_book['title']
     file_name = f'{title_comic_book}.png'
